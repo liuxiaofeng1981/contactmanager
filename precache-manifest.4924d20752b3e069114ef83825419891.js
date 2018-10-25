784d3fbc54d1addd01322f26a18ec5185913e2f7
liuxiaofeng1981 <liuxiaofeng1981@gmail.com>
2018-10-25 22:15:40 +1100

    Updates

---
+++ precache-manifest.4924d20752b3e069114ef83825419891.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "7f351aea0ce4a791bfe4",
+    "url": "/contactmanager/static/js/main.7f351aea.chunk.js"
+  },
+  {
+    "revision": "2974309e4007450276f6",
+    "url": "/contactmanager/static/js/1.2974309e.chunk.js"
+  },
+  {
+    "revision": "7f351aea0ce4a791bfe4",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "2974309e4007450276f6",
+    "url": "/contactmanager/static/css/1.6473ce2f.chunk.css"
+  },
+  {
+    "revision": "37c0d0bff669d0507e111965db4a2a94",
+    "url": "/contactmanager/index.html"
+  }
+];
